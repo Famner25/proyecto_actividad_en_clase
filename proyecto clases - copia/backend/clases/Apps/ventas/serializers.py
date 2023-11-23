a65c1c0f382657fde552cdab6ceb07b64ff4df96
Famner25 <fdanieldejesusgonzalez@uniguajira.edu.co>
2023-11-23 16:45:40 -0500

    este es el proyecto de la clase echo en oracle

---
+++ proyecto clases - copia/backend/clases/Apps/ventas/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Venta
+
+
+class VentasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venta
+        fields = '__all__'
---
+++ proyecto clases - copia/backend/clases/Apps/ventas/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Venta
+from .serializers import VentasSerializer
+
+
+
+class VentasViewSet (viewsets.ModelViewSet):
+    queryset = Venta.objects.all()
+    serializer_class = VentasSerializer
+
+
